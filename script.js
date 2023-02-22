2fd3d2ca9ac2b8bb9e2abe6d608bfb9b723c946b
Programist0 <frontendforpleasure@gmail.com>
2023-02-22 17:14:49 +0100

    Ver 1.5

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let openModal = () => {
     document.querySelector(".buy-modal").style.opacity = 1
+    document.querySelector(".buy-modal").style.zIndex = 1
 }
 
 let closeModal = () => {
     document.querySelector(".buy-modal").style.opacity = 0
+    document.querySelector(".buy-modal").style.zIndex = -1
 }
 
 document.querySelector(".buy-button").addEventListener("click", openModal)
